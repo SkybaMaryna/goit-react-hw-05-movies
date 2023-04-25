e62e0da8980a7412b258747f1463df6c2b384ad2
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-04-25 20:52:14 +0300

    summary

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { fetchMoviesReview } from 'services/moviesAPI';
 
 const Reviews = () => {
